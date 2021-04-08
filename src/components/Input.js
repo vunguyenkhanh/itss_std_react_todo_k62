c91b7cfdcd537fb49cd902c53f599a8b806fa7ca
EC2 Default User <ec2-user@ip-172-31-88-197.ec2.internal>
2021-04-08 04:14:21 +0000

    Add Tabs

--- src/components/Input.js
+++ src/components/Input.js
@@ -19,7 +19,7 @@ function Input( { onAdd } ) {
       <input
         class="input"
         type="text"
-        placeholder="Enter to add"
+        placeholder="Todoを入力してください"
         value={text}
         onChange={handleChange}
         onKeyDown={handleKeyDown}
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -27,6 +27,14 @@ function Todo() {
     /* テストコード 終了 */
   ]);
 
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+  
   const handleCheck = checked => {
     const newItems = items.map(item => {
       if (item.key === checked.key) {
@@ -41,13 +49,19 @@ function Todo() {
     putItems([...items, { key: getKey(), text, done: false }]);
   };
   
+  const handleFilterChange = value => setFilter(value);
+  
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
       <Input onAdd={handleAdd} />
-      {items.map(item => (
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
         <TodoItem
             key={item.key}
             item={item}
@@ -55,7 +69,7 @@ function Todo() {
         />
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
       </div>
     </div>
   );
